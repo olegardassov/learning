75bc590a41529d875bf8bf00d5b24e1ad9d4c7d3
olegardassov <78482277+olegardassov@users.noreply.github.com>
2021-02-27 23:50:32 +0200

    Test

--- homework_TOP15.py
+++ homework_TOP15.py
@@ -1,15 +1,16 @@
 import pandas as pd
 df = pd.read_csv("2019.csv", header=1)
 pd.set_option("display.max_columns", 10)
+pd.set_option("display.max_rows", 160)
 
 while True:
-    try: # checks if entered value is a number
+    try:  # checks if entered value is a number
         user_choice = input(
             "Please choose Column to use as a filter: \n1. Score \n2. GDP per capita \n3. Social support \n4. Healthy life expectancy \n5. Freedom to make life choices \n6. Generosity \n7. Perceptions of corruption \n-->: ")
         val = int(user_choice)
         top = input("Please input how long should your TOP be: ")
-        val2 = int(top)
-        filter = input("Please input, if ascedning should be  true or false: ").lower()
+        x = int(top)
+        filter = input("Please input, if ascending should be  true or false: ").lower()
         if filter == "true":
             y = True
             break
@@ -20,11 +21,9 @@ while True:
             print("ERROR. Please try again! ")
     except ValueError:
         print("That is not a number.")
-print(filter)
-x = int(top)
 if user_choice == '1':
     new = df.sort_values(by=["Score"], ascending=y).head(x)
-    print(df.sort_values(by=["Score"], ascending=y).head(x)), new.to_csv("result.csv", index=True) # saves output to "result.csv" file
+    print(df.sort_values(by=["Score"], ascending=y).head(x)), new.to_csv("result.csv", index=True)   # saves output to "result.csv" file
 elif user_choice == '2':
     new = df.sort_values(by=["GDP per capita"], ascending=y).head(x)
     print(df.sort_values(by=["GDP per capita"], ascending=y).head(x)), new.to_csv("result.csv")
@@ -44,6 +43,5 @@ elif user_choice == '7':
     new = df.sort_values(by=["Perceptions of corruption"], ascending=y).head(x)
     print(df.sort_values(by=["Perceptions of corruption"], ascending=y).head(x)), new.to_csv("result.csv")
 else:
-    print("Number you entered is out of range!")
-
+    print("Number of Column you entered is out of range!")
 
